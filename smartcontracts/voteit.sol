471168a655a24c5c33100c1b72655bcfa61e14c1
JustinBenito <justinbenito1974@gmail.com>
2023-11-21 22:25:41 +0530

    voting

---
+++ smartcontracts/voteit.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Voteit {
+
+    struct Voter {
+        bool hasVoted;
+    }
+
+    mapping(address => Voter) public voters;
+    uint public totalVotes;
+    uint public yesVotes;
+
+    address public admin;
+
+    enum State { Created, Voting, Ended }
+    State public state;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(state == _state, "Invalid state for this operation");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+        state = State.Created;
+    }
+
+    function startVoting() external onlyAdmin inState(State.Created) {
+        state = State.Voting;
+    }
+
+    function vote(bool _choice) external inState(State.Voting) returns (bool) {
+        require(!voters[msg.sender].hasVoted, "You have already voted");
+        
+        voters[msg.sender].hasVoted = true;
+        totalVotes++;
+
+        if (_choice) {
+            yesVotes++;
+        }
+
+        return true;
+    }
+
+    function endVoting() external onlyAdmin inState(State.Voting) {
+        state = State.Ended;
+    }
+}

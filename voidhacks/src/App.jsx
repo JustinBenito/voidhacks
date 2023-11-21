694daa715ce2a624bfe956a3277ec2132998fd90
JustinBenito <justinbenito1974@gmail.com>
2023-11-21 21:15:09 +0530

    smart contracts

--- voidhacks/src/App.jsx
+++ voidhacks/src/App.jsx
@@ -1,7 +1,8 @@
+import MetamaskBalance from "./wallet"
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <MetamaskBalance />
   )
 }
---
+++ voidhacks/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPBXAnAkWnEoqG9np9lkQ9AmcFSohYo-A",
+  authDomain: "voidhacks-bc1c3.firebaseapp.com",
+  projectId: "voidhacks-bc1c3",
+  storageBucket: "voidhacks-bc1c3.appspot.com",
+  messagingSenderId: "291657210402",
+  appId: "1:291657210402:web:ea12de8fc8f49f1c83887b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
